f06c9f400e1b76090df5eed65b3824f9fe5adc7a
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-06-07 21:04:54 -0500

    optimizacion

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
-
 from flask import Flask
 app = Flask(__name__)
+
+
+
+dp = [0 for i in range(100)]
 
 @app.route('/')
 def index():
     return "Hello"
 
 
-@app.route('/factorial/<int:n>')
 def factorial(n):
-    result = 1
-    for i in range(1,n+1):
-    	result *= i
-    return f"Factorial is: {result}"
+    if dp[n] != 0:
+        return dp[n]
+    else:
+        if n == 1 or n == 0:
+            dp[n] = 1
+        else:
+            dp[n] = n*factorial(n-1)
+
+        return dp[n]
+
+
+
+@app.route('/factorial/<int:n>')
+def print_factorial(n):
+    temp = len(dp) - 1
+    if n > len(dp):
+
+        while temp < n:
+            dp.append(0)
+            temp = temp + 1 
+    return f"Factorial is: {factorial(n)}"
+
 
 @app.route('/<name>')
 def my_view_func(name):
+
     return name
+
 
 if __name__ == "__main__":
     app.run(debug=True)
